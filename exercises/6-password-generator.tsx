79e2f1ded22753a34c629452fa7297344bb24932
Ralph Largo <devlargs@gmail.com>
2023-05-30 03:17:43 +0800

    feat: added random generator

--- exercises/6-password-generator.tsx
+++ exercises/6-password-generator.tsx
@@ -1,8 +1,11 @@
+// Instructions
+// Create a PasswordGenerator component that renders a password generator
+
 const PasswordGenerator = () => {
   return (
     <>
       <h1>Password Generator</h1>
-      <input disabled value="pR1" />
+      <input disabled value="pR1nC35S!_69" />
       <button>Copy</button>
       <br />
       Password Length:
---
+++ exercises/7-random-quote-generator.tsx
@@ -0,0 +1,13 @@
+// Instructions
+// Create a RandomQuoteGenerator component that renders a random quote from a famous person
+// Fetch data from ./data/quotes.js
+
+const RandomQuoteGenerator = () => {
+  return (
+    <>
+      <h1>Random Quote Generator</h1>
+    </>
+  );
+};
+
+export default RandomQuoteGenerator;
